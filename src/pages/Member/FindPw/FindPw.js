986312bc58765d89efb21330cbdbd2cf5b82a91c
김소현 <a6570407@naver.com>
2022-06-30 21:46:21 +0900

    Feat: "비밀번호 재설정 메일 발송 기능 추가"

--- src/pages/Member/FindPw/FindPw.js
+++ src/pages/Member/FindPw/FindPw.js
@@ -1,4 +1,36 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,8 +38,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 const Header_1 = __importDefault(require("../../../components/Header/Header"));
 const Footer_1 = __importDefault(require("../../../components/Footer/Footer"));
+const firestore_1 = require("firebase/firestore");
+const react_1 = require("react");
+const firebase_1 = __importStar(require("../../../firebase"));
+const auth_1 = require("firebase/auth");
 function Main() {
-    return ((0, jsx_runtime_1.jsx)("main", Object.assign({ className: "middle-main" }, { children: (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "middle-container" }, { children: [(0, jsx_runtime_1.jsx)("h1", Object.assign({ className: "find-title" }, { children: "FIND PW" })), (0, jsx_runtime_1.jsxs)("form", Object.assign({ className: "find-form" }, { children: [(0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "input-container" }, { children: [(0, jsx_runtime_1.jsx)("div", Object.assign({ className: "input-wrap" }, { children: (0, jsx_runtime_1.jsx)("input", { id: "userName", type: "text", placeholder: "\uC774\uB984" }) })), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "input-wrap" }, { children: (0, jsx_runtime_1.jsx)("input", { id: "userEmail", type: "text", placeholder: "\uC774\uBA54\uC77C" }) }))] })), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "btn-wrap" }, { children: (0, jsx_runtime_1.jsx)("button", Object.assign({ className: "find-btn", style: { marginTop: "20px" } }, { children: "\uC774\uBA54\uC77C \uC778\uC99D" })) }))] }))] })) })));
+    const [name, setName] = (0, react_1.useState)("");
+    const [email, setEmail] = (0, react_1.useState)("");
+    const [successMsg, setSuccessMsg] = (0, react_1.useState)("none");
+    const [errorMsg, setErrorMsg] = (0, react_1.useState)("none");
+    function fetchUser() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const q = (0, firestore_1.query)((0, firestore_1.collection)(firebase_1.default, "users"), (0, firestore_1.where)("name", "==", name), (0, firestore_1.where)("email", "==", email));
+            const userSnapshot = yield (0, firestore_1.getDocs)(q);
+            if (userSnapshot.size > 0) {
+                setErrorMsg("none");
+                (0, auth_1.sendPasswordResetEmail)(firebase_1.auth, email)
+                    .then(() => {
+                    // Password reset email sent!
+                    setSuccessMsg("block");
+                })
+                    .catch(error => {
+                    setSuccessMsg("none");
+                    alert("이메일 발송 중에 오류가 발생했습니다.\n" + error.message);
+                });
+            }
+            else {
+                setErrorMsg("block");
+            }
+        });
+    }
+    return ((0, jsx_runtime_1.jsx)("main", Object.assign({ className: "middle-main" }, { children: (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "middle-container" }, { children: [(0, jsx_runtime_1.jsx)("h1", Object.assign({ className: "find-title" }, { children: "FIND PW" })), (0, jsx_runtime_1.jsxs)("form", Object.assign({ className: "find-form" }, { children: [(0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "input-container" }, { children: [(0, jsx_runtime_1.jsx)("div", Object.assign({ className: "input-wrap" }, { children: (0, jsx_runtime_1.jsx)("input", { id: "userName", type: "text", placeholder: "\uC774\uB984", onChange: e => setName(e.target.value) }) })), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "input-wrap" }, { children: (0, jsx_runtime_1.jsx)("input", { id: "userEmail", type: "text", placeholder: "\uC774\uBA54\uC77C", onChange: e => setEmail(e.target.value) }) })), (0, jsx_runtime_1.jsx)("p", Object.assign({ className: "success-msg", style: { display: successMsg } }, { children: "\uBE44\uBC00\uBC88\uD638 \uC7AC\uC124\uC815\uC744 \uC704\uD55C \uC774\uBA54\uC77C\uC774 \uBC1C\uC1A1\uB418\uC5C8\uC2B5\uB2C8\uB2E4." })), (0, jsx_runtime_1.jsx)("p", Object.assign({ className: "error-msg", style: { display: errorMsg } }, { children: "\uC785\uB825\uD55C \uC815\uBCF4\uC640 \uC77C\uCE58\uD558\uB294 \uACC4\uC815\uC774 \uC5C6\uC2B5\uB2C8\uB2E4." }))] })), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "btn-wrap" }, { children: (0, jsx_runtime_1.jsx)("button", Object.assign({ type: "button", className: "find-btn", style: { marginTop: "20px" }, onClick: fetchUser, disabled: successMsg === "block" ? true : false }, { children: "\uC774\uBA54\uC77C \uC778\uC99D" })) }))] }))] })) })));
 }
 function FindId() {
     return ((0, jsx_runtime_1.jsxs)("div", { children: [(0, jsx_runtime_1.jsx)(Header_1.default, {}), (0, jsx_runtime_1.jsx)(Main, {}), (0, jsx_runtime_1.jsx)(Footer_1.default, {})] }));
--- src/pages/Member/FindPw/FindPw.tsx
+++ src/pages/Member/FindPw/FindPw.tsx
@@ -1,7 +1,37 @@
 import Header from "../../../components/Header/Header";
 import Footer from "../../../components/Footer/Footer";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useState } from "react";
+import db, { auth } from "../../../firebase";
+import { sendPasswordResetEmail } from "firebase/auth";
 
 function Main() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [successMsg, setSuccessMsg] = useState("none");
+    const [errorMsg, setErrorMsg] = useState("none");
+
+    async function fetchUser() {
+        const q = query(collection(db, "users"), where("name", "==", name), where("email", "==", email));
+        const userSnapshot = await getDocs(q);
+
+        if (userSnapshot.size > 0) {
+            setErrorMsg("none");
+
+            sendPasswordResetEmail(auth, email)
+                .then(() => {
+                    // Password reset email sent!
+                    setSuccessMsg("block");
+                })
+                .catch(error => {
+                    setSuccessMsg("none");
+                    alert("이메일 발송 중에 오류가 발생했습니다.\n" + error.message);
+                });
+        } else {
+            setErrorMsg("block");
+        }
+    }
+
     return (
         <main className="middle-main">
             <div className="middle-container">
@@ -9,14 +39,20 @@ function Main() {
                 <form className="find-form">
                     <div className="input-container">
                         <div className="input-wrap">
-                            <input id="userName" type="text" placeholder="이름" />
+                            <input id="userName" type="text" placeholder="이름" onChange={e => setName(e.target.value)} />
                         </div>
                         <div className="input-wrap">
-                            <input id="userEmail" type="text" placeholder="이메일" />
+                            <input id="userEmail" type="text" placeholder="이메일" onChange={e => setEmail(e.target.value)} />
                         </div>
+                        <p className="success-msg" style={{ display: successMsg }}>
+                            비밀번호 재설정을 위한 이메일이 발송되었습니다.
+                        </p>
+                        <p className="error-msg" style={{ display: errorMsg }}>
+                            입력한 정보와 일치하는 계정이 없습니다.
+                        </p>
                     </div>
                     <div className="btn-wrap">
-                        <button className="find-btn" style={{ marginTop: "20px" }}>
+                        <button type="button" className="find-btn" style={{ marginTop: "20px" }} onClick={fetchUser} disabled={successMsg === "block" ? true : false}>
                             이메일 인증
                         </button>
                     </div>
