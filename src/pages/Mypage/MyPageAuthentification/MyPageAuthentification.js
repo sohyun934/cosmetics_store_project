6a52b1440bef959e602574bf1bb7d28a72fe49f5
김소현 <a6570407@naver.com>
2022-07-17 23:11:49 +0900

    Feat: "비밀번호 확인 기능 추가"

--- src/pages/Mypage/MyPageAuthentification/MyPageAuthentification.js
+++ src/pages/Mypage/MyPageAuthentification/MyPageAuthentification.js
@@ -6,8 +6,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 const Header_1 = __importDefault(require("../../../components/Header/Header"));
 const Footer_1 = __importDefault(require("../../../components/Footer/Footer"));
+const react_router_dom_1 = require("react-router-dom");
+const react_1 = require("react");
+const auth_1 = require("firebase/auth");
+const firebase_1 = require("../../../firebase");
 function Main() {
-    return ((0, jsx_runtime_1.jsx)("main", Object.assign({ className: "middle-main" }, { children: (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "middle-container" }, { children: [(0, jsx_runtime_1.jsx)("h1", Object.assign({ className: "section-title" }, { children: "\uBE44\uBC00\uBC88\uD638 \uD655\uC778" })), (0, jsx_runtime_1.jsxs)("form", Object.assign({ className: "auth-form", action: "#", method: "post" }, { children: [(0, jsx_runtime_1.jsx)("p", { children: "\uAC1C\uC778 \uC815\uBCF4\uB97C \uBCF4\uD638\uD558\uAE30 \uC704\uD574 \uBE44\uBC00\uBC88\uD638\uB97C \uB2E4\uC2DC \uC785\uB825\uD574 \uC8FC\uC138\uC694." }), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "input-wrap", style: { padding: "24px 0" } }, { children: (0, jsx_runtime_1.jsx)("input", { id: "userPw", type: "password", placeholder: "\uBE44\uBC00\uBC88\uD638" }) })), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "btn-wrap" }, { children: (0, jsx_runtime_1.jsx)("button", Object.assign({ className: "auth-btn" }, { children: "\uD655\uC778" })) }))] }))] })) })));
+    const [password, setPassword] = (0, react_1.useState)("");
+    const [errorMsg, setErrorMsg] = (0, react_1.useState)("");
+    const user = firebase_1.auth.currentUser;
+    const credential = auth_1.EmailAuthProvider.credential(user.email, password);
+    const navigate = (0, react_router_dom_1.useNavigate)();
+    function passwordChk() {
+        (0, auth_1.reauthenticateWithCredential)(user, credential)
+            .then(() => {
+            navigate("/mypage/modify", {
+                state: {
+                    password: password
+                }
+            });
+        })
+            .catch(error => {
+            setErrorMsg("비밀번호가 일치하지 않습니다.");
+        });
+    }
+    return ((0, jsx_runtime_1.jsx)("main", Object.assign({ className: "middle-main" }, { children: (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "middle-container" }, { children: [(0, jsx_runtime_1.jsx)("h1", Object.assign({ className: "section-title" }, { children: "\uBE44\uBC00\uBC88\uD638 \uD655\uC778" })), (0, jsx_runtime_1.jsxs)("form", Object.assign({ className: "auth-form", action: "#", method: "post" }, { children: [(0, jsx_runtime_1.jsx)("p", { children: "\uAC1C\uC778 \uC815\uBCF4\uB97C \uBCF4\uD638\uD558\uAE30 \uC704\uD574 \uBE44\uBC00\uBC88\uD638\uB97C \uB2E4\uC2DC \uC785\uB825\uD574 \uC8FC\uC138\uC694." }), (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "input-wrap", style: { padding: "24px 0" } }, { children: [(0, jsx_runtime_1.jsx)("input", { id: "userPw", type: "password", placeholder: "\uBE44\uBC00\uBC88\uD638", onChange: e => {
+                                        setPassword(e.target.value);
+                                        setErrorMsg("");
+                                    } }), (0, jsx_runtime_1.jsx)("p", Object.assign({ className: "error-msg" }, { children: errorMsg }))] })), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "btn-wrap" }, { children: (0, jsx_runtime_1.jsx)("button", Object.assign({ type: "button", className: "auth-btn", onClick: passwordChk }, { children: "\uD655\uC778" })) }))] }))] })) })));
 }
 function MyPageAuthentification() {
     return ((0, jsx_runtime_1.jsxs)("div", { children: [(0, jsx_runtime_1.jsx)(Header_1.default, {}), (0, jsx_runtime_1.jsx)(Main, {}), (0, jsx_runtime_1.jsx)(Footer_1.default, {})] }));
--- src/pages/Mypage/MyPageAuthentification/MyPageAuthentification.tsx
+++ src/pages/Mypage/MyPageAuthentification/MyPageAuthentification.tsx
@@ -1,7 +1,32 @@
 import Header from "../../../components/Header/Header";
 import Footer from "../../../components/Footer/Footer";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { EmailAuthProvider, onAuthStateChanged, reauthenticateWithCredential, signInWithEmailAndPassword } from "firebase/auth";
+import { auth, signedInUser } from "../../../firebase";
 
 function Main() {
+    const [password, setPassword] = useState("");
+    const [errorMsg, setErrorMsg] = useState("");
+
+    const user = auth.currentUser;
+    const credential = EmailAuthProvider.credential(user.email, password);
+    const navigate = useNavigate();
+
+    function passwordChk() {
+        reauthenticateWithCredential(user, credential)
+            .then(() => {
+                navigate("/mypage/modify", {
+                    state: {
+                        password: password
+                    }
+                });
+            })
+            .catch(error => {
+                setErrorMsg("비밀번호가 일치하지 않습니다.");
+            });
+    }
+
     return (
         <main className="middle-main">
             <div className="middle-container">
@@ -9,10 +34,21 @@ function Main() {
                 <form className="auth-form" action="#" method="post">
                     <p>개인 정보를 보호하기 위해 비밀번호를 다시 입력해 주세요.</p>
                     <div className="input-wrap" style={{ padding: "24px 0" }}>
-                        <input id="userPw" type="password" placeholder="비밀번호" />
+                        <input
+                            id="userPw"
+                            type="password"
+                            placeholder="비밀번호"
+                            onChange={e => {
+                                setPassword(e.target.value);
+                                setErrorMsg("");
+                            }}
+                        />
+                        <p className="error-msg">{errorMsg}</p>
                     </div>
                     <div className="btn-wrap">
-                        <button className="auth-btn">확인</button>
+                        <button type="button" className="auth-btn" onClick={passwordChk}>
+                            확인
+                        </button>
                     </div>
                 </form>
             </div>
