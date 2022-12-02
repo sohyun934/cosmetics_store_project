857113235d2308328e4f2f3ae1aa5be4d5ab2021
김소현 <a6570407@naver.com>
2022-12-02 16:57:49 +0900

    Refactor: "상품 리스트 코드 리팩토링"
    가독성 향상

--- src/components/ProductMain/ProductMain.js
+++ src/components/ProductMain/ProductMain.js
@@ -21,6 +21,13 @@ const firebase_1 = require("../../firebase");
 const getImage_1 = require("../../utils/getImage");
 const styled_components_1 = __importDefault(require("styled-components"));
 const CartPop_1 = __importDefault(require("../../pages/Mypage/Cart/CartPop/CartPop"));
+const Category = (props) => {
+    const category = props.category;
+    const pathname = props.pathname;
+    const title = category[pathname].title;
+    const desc = category[pathname].desc;
+    return ((0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "section-title" }, { children: [(0, jsx_runtime_1.jsx)("h1", { children: title }), (0, jsx_runtime_1.jsx)("p", Object.assign({ style: { wordBreak: "keep-all" } }, { children: desc }))] })));
+};
 const StyledButton = styled_components_1.default.button `
     @media (min-width: 900px) {
         width: 30%;
@@ -64,96 +71,81 @@ const SelectItem = styled_components_1.default.li `
         background-color: #e5e5e5;
     }
 `;
-function ProductMain() {
-    const [pop, setPop] = (0, react_1.useState)({ state: "", content: null });
-    const [products, setProducts] = (0, react_1.useState)([]);
+function ProductList(props) {
+    const pathname = props.pathname;
+    const [cartPop, setCartPop] = (0, react_1.useState)({ state: "", desc: null });
     const [moreBtn, setMoreBtn] = (0, react_1.useState)(false);
     const [lastVisible, setLastVisible] = (0, react_1.useState)(null);
-    const [order, setOrder] = (0, react_1.useState)({ field: "product_id", direction: "desc" });
+    const [products, setProducts] = (0, react_1.useState)([]);
     const [selectItem, setSelectItem] = (0, react_1.useState)("등록순");
-    const selectListRef = (0, react_1.useRef)(null);
-    const selectBtnRef = (0, react_1.useRef)(null);
+    const [order, setOrder] = (0, react_1.useState)({ field: "product_id", direction: "desc" });
     const navigate = (0, react_router_dom_1.useNavigate)();
-    const pathname = (0, react_router_dom_1.useLocation)().pathname.substring(1);
-    const limitNum = 9;
-    const category = {
-        hair: {
-            title: "HAIR CARE",
-            desc: "다양한 헤어케어를 경험해보세요"
-        },
-        skin: {
-            title: "SKIN CARE",
-            desc: "건강한 피부를 만드는 스킨케어를 만나보세요"
-        },
-        body: {
-            title: "BODY CARE",
-            desc: "일상의 무게를 줄여주는 바디케어를 경험해보세요"
-        }
-    };
+    const selectBtnRef = (0, react_1.useRef)(null);
+    const selectListRef = (0, react_1.useRef)(null);
     const selectItemList = ["등록순", "낮은가격순", "높은가격순"];
     const selectItems = selectItemList.map(item => ((0, jsx_runtime_1.jsx)(SelectItem, Object.assign({ onClick: e => handleSort(e), className: item === selectItem ? "active" : "" }, { children: item }), item)));
+    // 장바구니 팝업창 제어
     const closePop = () => {
-        setPop(pop => (Object.assign(Object.assign({}, pop), { state: "", content: null })));
+        setCartPop(cartPop => (Object.assign(Object.assign({}, cartPop), { state: "", desc: null })));
     };
     (0, react_1.useEffect)(() => {
-        if (pop.state === "cart") {
-            setPop(pop => (Object.assign(Object.assign({}, pop), { content: (0, jsx_runtime_1.jsx)(CartPop_1.default, { close: closePop, title: "\uC0C1\uD488\uC774 \uC7A5\uBC14\uAD6C\uB2C8\uC5D0 \uB2F4\uACBC\uC2B5\uB2C8\uB2E4." }) })));
+        if (cartPop.state) {
+            setCartPop(cartPop => (Object.assign(Object.assign({}, cartPop), { desc: ((0, jsx_runtime_1.jsx)(CartPop_1.default, { close: closePop, title: cartPop.state === "overlap" ? "이미 장바구니에 담겨있는 상품입니다." : "상품이 장바구니에 담겼습니다." })) })));
         }
-        else if (pop.state === "overlap") {
-            setPop(pop => (Object.assign(Object.assign({}, pop), { content: (0, jsx_runtime_1.jsx)(CartPop_1.default, { close: closePop, title: "\uC774\uBBF8 \uC7A5\uBC14\uAD6C\uB2C8\uC5D0 \uB2F4\uACA8\uC788\uB294 \uC0C1\uD488\uC785\uB2C8\uB2E4." }) })));
-        }
-    }, [pop.state]);
+    }, [cartPop.state]);
     const handleAddCart = (productName) => __awaiter(this, void 0, void 0, function* () {
         if (!firebase_1.signedInUser) {
             navigate("/login", { state: { moveTo: -1 } });
         }
         else {
-            const cartList = yield (0, firestore_1.getDocs)((0, firestore_1.query)((0, firestore_1.collection)(firebase_1.db, "cart"), (0, firestore_1.where)("user_email", "==", firebase_1.signedInUser)));
             const q = (0, firestore_1.query)((0, firestore_1.collection)(firebase_1.db, "cart"), (0, firestore_1.where)("user_email", "==", firebase_1.signedInUser), (0, firestore_1.where)("product_name", "==", productName));
-            const querySnapshot = yield (0, firestore_1.getDocs)(q);
-            if (querySnapshot.empty) {
+            const cartItem = yield (0, firestore_1.getDocs)(q);
+            const cartList = yield (0, firestore_1.getDocs)((0, firestore_1.query)((0, firestore_1.collection)(firebase_1.db, "cart"), (0, firestore_1.where)("user_email", "==", firebase_1.signedInUser)));
+            if (cartItem.empty) {
                 yield (0, firestore_1.addDoc)((0, firestore_1.collection)(firebase_1.db, "cart"), {
                     cart_id: cartList.size + 1,
                     product_name: productName,
                     user_email: firebase_1.signedInUser,
                     amount: 1
                 });
-                setPop(pop => (Object.assign(Object.assign({}, pop), { state: "cart" })));
+                setCartPop(cartPop => (Object.assign(Object.assign({}, cartPop), { state: "new" })));
             }
             else {
-                setPop(pop => (Object.assign(Object.assign({}, pop), { state: "overlap" })));
+                setCartPop(cartPop => (Object.assign(Object.assign({}, cartPop), { state: "overlap" })));
             }
         }
     });
     // 상품 리스트 가져오기
-    const fetchProducts = (field, direction, isClicked) => __awaiter(this, void 0, void 0, function* () {
+    const fetchProducts = (field, direction, isMoreBtnClicked) => __awaiter(this, void 0, void 0, function* () {
+        const limitNum = 9;
         let productSnapshot;
         let productList;
-        // 더보기 버튼을 클릭한 경우
-        if (isClicked) {
+        if (isMoreBtnClicked) {
+            // 상품 추가로 가져오기
+            const moreQ = (0, firestore_1.query)((0, firestore_1.collection)(firebase_1.db, "product"), (0, firestore_1.where)("product_type", "==", pathname), (0, firestore_1.orderBy)(field, direction), (0, firestore_1.startAfter)(lastVisible), (0, firestore_1.limit)(limitNum));
+            productSnapshot = yield (0, firestore_1.getDocs)(moreQ);
+            productList = [...products];
             // 나머지 상품 갯수가 limit 이하인 경우 더보기 버튼 감추기
             const q = (0, firestore_1.query)((0, firestore_1.collection)(firebase_1.db, "product"), (0, firestore_1.where)("product_type", "==", pathname), (0, firestore_1.orderBy)(field, direction), (0, firestore_1.startAfter)(lastVisible));
             const restProducts = yield (0, firestore_1.getDocs)(q);
-            if (restProducts.size <= limitNum)
+            if (restProducts.size <= limitNum) {
                 setMoreBtn(false);
-            // 상품 추가로 가져오기
-            const more = (0, firestore_1.query)((0, firestore_1.collection)(firebase_1.db, "product"), (0, firestore_1.where)("product_type", "==", pathname), (0, firestore_1.orderBy)(field, direction), (0, firestore_1.startAfter)(lastVisible), (0, firestore_1.limit)(limitNum));
-            productSnapshot = yield (0, firestore_1.getDocs)(more);
-            productList = [...products];
+            }
         }
         else {
-            const q = (0, firestore_1.query)((0, firestore_1.collection)(firebase_1.db, "product"), (0, firestore_1.where)("product_type", "==", pathname));
-            const totProducts = yield (0, firestore_1.getDocs)(q);
-            // 전체 상품 갯수가 limit을 초과하는 경우 더보기 버튼 노출
-            if (totProducts.size > limitNum) {
-                setMoreBtn(true);
-            }
             // 상품 가져오기
             const first = (0, firestore_1.query)((0, firestore_1.collection)(firebase_1.db, "product"), (0, firestore_1.where)("product_type", "==", pathname), (0, firestore_1.orderBy)(field, direction), (0, firestore_1.limit)(limitNum));
             productSnapshot = yield (0, firestore_1.getDocs)(first);
             productList = [];
+            // 전체 상품 갯수가 limit을 초과하는 경우 더보기 버튼 노출
+            const q = (0, firestore_1.query)((0, firestore_1.collection)(firebase_1.db, "product"), (0, firestore_1.where)("product_type", "==", pathname));
+            const totProducts = yield (0, firestore_1.getDocs)(q);
+            if (totProducts.size > limitNum) {
+                setMoreBtn(true);
+            }
         }
         setLastVisible(productSnapshot.docs[productSnapshot.docs.length - 1]);
+        // 상품 리스트 만들기
         if (productSnapshot.empty) {
             productList.push((0, jsx_runtime_1.jsx)("li", Object.assign({ style: { padding: "20px", marginTop: "50px", marginBottom: "40vh" } }, { children: "\uB4F1\uB85D\uB41C \uC0C1\uD488\uC774 \uC5C6\uC2B5\uB2C8\uB2E4." })));
         }
@@ -161,7 +153,6 @@ function ProductMain() {
             // storage 이미지 가져오기
             const promises = productSnapshot.docs.map(doc => (0, getImage_1.getImage)(doc.data().product_thumb_01));
             const urls = yield Promise.all(promises);
-            // firestore 데이터 가져와서 리스트 만들기
             productSnapshot.docs.forEach((doc, i) => {
                 const data = doc.data();
                 productList.push((0, jsx_runtime_1.jsxs)("li", Object.assign({ className: "product" }, { children: [(0, jsx_runtime_1.jsxs)(react_router_dom_1.Link, Object.assign({ to: "/detail", state: {
@@ -177,39 +168,58 @@ function ProductMain() {
         setProducts(productList);
     });
     (0, react_1.useEffect)(() => {
+        // 상품 리스트 출력
         fetchProducts("product_id", "desc", false);
     }, []);
+    // 상품 리스트 정렬 제어
     const handleSelect = () => {
-        const selectList = selectListRef.current;
         const selectBtn = selectBtnRef.current;
+        const selectList = selectListRef.current;
         selectList.classList.toggle("display-none");
         selectBtn.classList.toggle("unactive");
     };
-    // 상품 리스트 정렬
     const handleSort = (e) => {
+        const selectItem = e.target.innerText;
         const selectList = selectListRef.current;
-        const selectItemContent = e.target.innerText;
+        selectList.classList.toggle("display-none");
         let field = "product_id";
         let direction = "desc";
-        selectList.classList.toggle("display-none");
-        if (selectItemContent === "등록순") {
+        if (selectItem === "등록순") {
             field = "product_id";
             direction = "desc";
         }
-        else if (selectItemContent === "낮은가격순") {
+        else if (selectItem === "낮은가격순") {
             field = "product_price";
             direction = "asc";
         }
-        else if (selectItemContent === "높은가격순") {
+        else if (selectItem === "높은가격순") {
             field = "product_price";
             direction = "desc";
         }
-        fetchProducts(field, direction, false);
-        setSelectItem(selectItemContent);
+        setSelectItem(selectItem);
         setOrder(orderBy => (Object.assign(Object.assign({}, orderBy), { field: field, direction: direction })));
+        fetchProducts(field, direction, false);
     };
-    return ((0, jsx_runtime_1.jsxs)("main", { children: [(0, jsx_runtime_1.jsxs)("section", Object.assign({ className: "product-section" }, { children: [(0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "section-title" }, { children: [(0, jsx_runtime_1.jsx)("h1", { children: category[pathname].title }), (0, jsx_runtime_1.jsx)("p", Object.assign({ style: { wordBreak: "keep-all" } }, { children: category[pathname].desc }))] })), (0, jsx_runtime_1.jsxs)(SelectBox, { children: [(0, jsx_runtime_1.jsx)(SelectBtn, Object.assign({ className: "unactive", onClick: handleSelect, ref: selectBtnRef }, { children: selectItem })), (0, jsx_runtime_1.jsx)(SelectList, Object.assign({ className: "display-none", ref: selectListRef }, { children: selectItems }))] }), (0, jsx_runtime_1.jsx)("ul", Object.assign({ className: "product-list flex" }, { children: products })), moreBtn && ((0, jsx_runtime_1.jsx)("div", Object.assign({ style: { textAlign: "center", padding: "0 20px" } }, { children: (0, jsx_runtime_1.jsx)(StyledButton, Object.assign({ className: "border-style-btn", onClick: () => {
-                                fetchProducts(order.field, order.direction, true);
-                            } }, { children: "\uB354\uBCF4\uAE30" })) })))] })), pop.content, (0, jsx_runtime_1.jsx)(MoveTop_1.default, {})] }));
+    return ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [(0, jsx_runtime_1.jsxs)(SelectBox, { children: [(0, jsx_runtime_1.jsx)(SelectBtn, Object.assign({ className: "unactive", onClick: handleSelect, ref: selectBtnRef }, { children: selectItem })), (0, jsx_runtime_1.jsx)(SelectList, Object.assign({ className: "display-none", ref: selectListRef }, { children: selectItems }))] }), (0, jsx_runtime_1.jsx)("ul", Object.assign({ className: "product-list flex" }, { children: products })), moreBtn && ((0, jsx_runtime_1.jsx)("div", Object.assign({ style: { textAlign: "center", padding: "0 20px" } }, { children: (0, jsx_runtime_1.jsx)(StyledButton, Object.assign({ className: "border-style-btn", onClick: () => {
+                        fetchProducts(order.field, order.direction, true);
+                    } }, { children: "\uB354\uBCF4\uAE30" })) }))), cartPop.desc] }));
 }
+const ProductMain = () => {
+    const pathname = (0, react_router_dom_1.useLocation)().pathname.substring(1);
+    const category = {
+        hair: {
+            title: "HAIR CARE",
+            desc: "다양한 헤어케어를 경험해보세요"
+        },
+        skin: {
+            title: "SKIN CARE",
+            desc: "건강한 피부를 만드는 스킨케어를 만나보세요"
+        },
+        body: {
+            title: "BODY CARE",
+            desc: "일상의 무게를 줄여주는 바디케어를 경험해보세요"
+        }
+    };
+    return ((0, jsx_runtime_1.jsxs)("main", { children: [(0, jsx_runtime_1.jsxs)("section", Object.assign({ className: "product-section" }, { children: [(0, jsx_runtime_1.jsx)(Category, { category: category, pathname: pathname }), (0, jsx_runtime_1.jsx)(ProductList, { pathname: pathname })] })), (0, jsx_runtime_1.jsx)(MoveTop_1.default, {})] }));
+};
 exports.default = ProductMain;
--- src/components/ProductMain/ProductMain.tsx
+++ src/components/ProductMain/ProductMain.tsx
@@ -20,6 +20,25 @@ import { getImage } from "../../utils/getImage";
 import styled from "styled-components";
 import CartPop from "../../pages/Mypage/Cart/CartPop/CartPop";
 
+type CategoryProp = {
+    category: {};
+    pathname: string;
+};
+
+const Category = (props: CategoryProp) => {
+    const category = props.category;
+    const pathname = props.pathname;
+    const title = category[pathname].title;
+    const desc = category[pathname].desc;
+
+    return (
+        <div className="section-title">
+            <h1>{title}</h1>
+            <p style={{ wordBreak: "keep-all" }}>{desc}</p>
+        </div>
+    );
+};
+
 const StyledButton = styled.button`
     @media (min-width: 900px) {
         width: 30%;
@@ -68,38 +87,28 @@ const SelectItem = styled.li`
     }
 `;
 
+type ListProps = {
+    pathname: string;
+};
+
 interface Order {
     field: string | FieldPath;
     direction: OrderByDirection;
 }
 
-function ProductMain() {
-    const [pop, setPop] = useState({ state: "", content: null });
-    const [products, setProducts] = useState([]);
+function ProductList(props: ListProps) {
+    const pathname = props.pathname;
+
+    const [cartPop, setCartPop] = useState({ state: "", desc: null });
     const [moreBtn, setMoreBtn] = useState(false);
     const [lastVisible, setLastVisible] = useState(null);
-    const [order, setOrder] = useState<Order>({ field: "product_id", direction: "desc" });
+    const [products, setProducts] = useState([]);
     const [selectItem, setSelectItem] = useState("등록순");
+    const [order, setOrder] = useState<Order>({ field: "product_id", direction: "desc" });
 
-    const selectListRef = useRef(null);
-    const selectBtnRef = useRef(null);
     const navigate = useNavigate();
-    const pathname = useLocation().pathname.substring(1);
-
-    const category = {
-        hair: {
-            title: "HAIR CARE",
-            desc: "다양한 헤어케어를 경험해보세요"
-        },
-        skin: {
-            title: "SKIN CARE",
-            desc: "건강한 피부를 만드는 스킨케어를 만나보세요"
-        },
-        body: {
-            title: "BODY CARE",
-            desc: "일상의 무게를 줄여주는 바디케어를 경험해보세요"
-        }
-    };
+    const selectBtnRef = useRef(null);
+    const selectListRef = useRef(null);
 
     const selectItemList = ["등록순", "낮은가격순", "높은가격순"];
     const selectItems = selectItemList.map(item => (
@@ -108,27 +117,31 @@ function ProductMain() {
         </SelectItem>
     ));
 
+    // 장바구니 팝업창 제어
     const closePop = () => {
-        setPop(pop => ({ ...pop, state: "", content: null }));
+        setCartPop(cartPop => ({ ...cartPop, state: "", desc: null }));
     };
 
     useEffect(() => {
-        if (pop.state === "cart") {
-            setPop(pop => ({ ...pop, content: <CartPop close={closePop} title="상품이 장바구니에 담겼습니다." /> }));
-        } else if (pop.state === "overlap") {
-            setPop(pop => ({ ...pop, content: <CartPop close={closePop} title="이미 장바구니에 담겨있는 상품입니다." /> }));
+        if (cartPop.state) {
+            setCartPop(cartPop => ({
+                ...cartPop,
+                desc: (
+                    <CartPop close={closePop} title={cartPop.state === "overlap" ? "이미 장바구니에 담겨있는 상품입니다." : "상품이 장바구니에 담겼습니다."} />
+                )
+            }));
         }
-    }, [pop.state]);
+    }, [cartPop.state]);
 
     const handleAddCart = async (productName: string) => {
         if (!signedInUser) {
             navigate("/login", { state: { moveTo: -1 } });
         } else {
-            const cartList = await getDocs(query(collection(db, "cart"), where("user_email", "==", signedInUser)));
             const q = query(collection(db, "cart"), where("user_email", "==", signedInUser), where("product_name", "==", productName));
-            const querySnapshot = await getDocs(q);
+            const cartItem = await getDocs(q);
+            const cartList = await getDocs(query(collection(db, "cart"), where("user_email", "==", signedInUser)));
 
-            if (querySnapshot.empty) {
+            if (cartItem.empty) {
                 await addDoc(collection(db, "cart"), {
                     cart_id: cartList.size + 1,
                     product_name: productName,
@@ -136,28 +149,22 @@ function ProductMain() {
                     amount: 1
                 });
 
-                setPop(pop => ({ ...pop, state: "cart" }));
+                setCartPop(cartPop => ({ ...cartPop, state: "new" }));
             } else {
-                setPop(pop => ({ ...pop, state: "overlap" }));
+                setCartPop(cartPop => ({ ...cartPop, state: "overlap" }));
             }
         }
     };
 
     // 상품 리스트 가져오기
-    const fetchProducts = async (field: string | FieldPath, direction: OrderByDirection, isClicked: boolean) => {
+    const fetchProducts = async (field: string | FieldPath, direction: OrderByDirection, isMoreBtnClicked: boolean) => {
         const limitNum = 9;
         let productSnapshot: QuerySnapshot<DocumentData>;
         let productList: any[];
 
-        // 더보기 버튼을 클릭한 경우
-        if (isClicked) {
-            // 나머지 상품 갯수가 limit 이하인 경우 더보기 버튼 감추기
-            const q = query(collection(db, "product"), where("product_type", "==", pathname), orderBy(field, direction), startAfter(lastVisible));
-            const restProducts = await getDocs(q);
-            if (restProducts.size <= limitNum) setMoreBtn(false);
-
+        if (isMoreBtnClicked) {
             // 상품 추가로 가져오기
-            const more = query(
+            const moreQ = query(
                 collection(db, "product"),
                 where("product_type", "==", pathname),
                 orderBy(field, direction),
@@ -165,25 +172,34 @@ function ProductMain() {
                 limit(limitNum)
             );
 
-            productSnapshot = await getDocs(more);
+            productSnapshot = await getDocs(moreQ);
             productList = [...products];
-        } else {
-            const q = query(collection(db, "product"), where("product_type", "==", pathname));
-            const totProducts = await getDocs(q);
 
-            // 전체 상품 갯수가 limit을 초과하는 경우 더보기 버튼 노출
-            if (totProducts.size > limitNum) {
-                setMoreBtn(true);
+            // 나머지 상품 갯수가 limit 이하인 경우 더보기 버튼 감추기
+            const q = query(collection(db, "product"), where("product_type", "==", pathname), orderBy(field, direction), startAfter(lastVisible));
+            const restProducts = await getDocs(q);
+
+            if (restProducts.size <= limitNum) {
+                setMoreBtn(false);
             }
-
+        } else {
             // 상품 가져오기
             const first = query(collection(db, "product"), where("product_type", "==", pathname), orderBy(field, direction), limit(limitNum));
             productSnapshot = await getDocs(first);
             productList = [];
+
+            // 전체 상품 갯수가 limit을 초과하는 경우 더보기 버튼 노출
+            const q = query(collection(db, "product"), where("product_type", "==", pathname));
+            const totProducts = await getDocs(q);
+
+            if (totProducts.size > limitNum) {
+                setMoreBtn(true);
+            }
         }
 
         setLastVisible(productSnapshot.docs[productSnapshot.docs.length - 1]);
 
+        // 상품 리스트 만들기
         if (productSnapshot.empty) {
             productList.push(<li style={{ padding: "20px", marginTop: "50px", marginBottom: "40vh" }}>등록된 상품이 없습니다.</li>);
         } else {
@@ -191,7 +207,6 @@ function ProductMain() {
             const promises = productSnapshot.docs.map(doc => getImage(doc.data().product_thumb_01));
             const urls = await Promise.all(promises);
 
-            // firestore 데이터 가져와서 리스트 만들기
             productSnapshot.docs.forEach((doc, i) => {
                 const data = doc.data();
 
@@ -234,74 +249,96 @@ function ProductMain() {
     };
 
     useEffect(() => {
+        // 상품 리스트 출력
         fetchProducts("product_id", "desc", false);
     }, []);
 
+    // 상품 리스트 정렬 제어
     const handleSelect = () => {
-        const selectList = selectListRef.current;
         const selectBtn = selectBtnRef.current;
+        const selectList = selectListRef.current;
         selectList.classList.toggle("display-none");
         selectBtn.classList.toggle("unactive");
     };
 
-    // 상품 리스트 정렬
     const handleSort = (e: React.MouseEvent<HTMLElement>) => {
+        const selectItem = (e.target as HTMLElement).innerText;
         const selectList = selectListRef.current;
-        const selectItemContent = (e.target as HTMLElement).innerText;
+        selectList.classList.toggle("display-none");
+
         let field: string | FieldPath = "product_id";
         let direction: OrderByDirection = "desc";
 
-        selectList.classList.toggle("display-none");
-
-        if (selectItemContent === "등록순") {
+        if (selectItem === "등록순") {
             field = "product_id";
             direction = "desc";
-        } else if (selectItemContent === "낮은가격순") {
+        } else if (selectItem === "낮은가격순") {
             field = "product_price";
             direction = "asc";
-        } else if (selectItemContent === "높은가격순") {
+        } else if (selectItem === "높은가격순") {
             field = "product_price";
             direction = "desc";
         }
 
-        fetchProducts(field, direction, false);
-        setSelectItem(selectItemContent);
+        setSelectItem(selectItem);
         setOrder(orderBy => ({ ...orderBy, field: field, direction: direction }));
+        fetchProducts(field, direction, false);
+    };
+
+    return (
+        <>
+            <SelectBox>
+                <SelectBtn className="unactive" onClick={handleSelect} ref={selectBtnRef}>
+                    {selectItem}
+                </SelectBtn>
+                <SelectList className="display-none" ref={selectListRef}>
+                    {selectItems}
+                </SelectList>
+            </SelectBox>
+            <ul className="product-list flex">{products}</ul>
+            {moreBtn && (
+                <div style={{ textAlign: "center", padding: "0 20px" }}>
+                    <StyledButton
+                        className="border-style-btn"
+                        onClick={() => {
+                            fetchProducts(order.field, order.direction, true);
+                        }}
+                    >
+                        더보기
+                    </StyledButton>
+                </div>
+            )}
+            {cartPop.desc}
+        </>
+    );
+}
+
+const ProductMain = () => {
+    const pathname = useLocation().pathname.substring(1);
+    const category = {
+        hair: {
+            title: "HAIR CARE",
+            desc: "다양한 헤어케어를 경험해보세요"
+        },
+        skin: {
+            title: "SKIN CARE",
+            desc: "건강한 피부를 만드는 스킨케어를 만나보세요"
+        },
+        body: {
+            title: "BODY CARE",
+            desc: "일상의 무게를 줄여주는 바디케어를 경험해보세요"
+        }
     };
 
     return (
         <main>
             <section className="product-section">
-                <div className="section-title">
-                    <h1>{category[pathname].title}</h1>
-                    <p style={{ wordBreak: "keep-all" }}>{category[pathname].desc}</p>
-                </div>
-                <SelectBox>
-                    <SelectBtn className="unactive" onClick={handleSelect} ref={selectBtnRef}>
-                        {selectItem}
-                    </SelectBtn>
-                    <SelectList className="display-none" ref={selectListRef}>
-                        {selectItems}
-                    </SelectList>
-                </SelectBox>
-                <ul className="product-list flex">{products}</ul>
-                {moreBtn && (
-                    <div style={{ textAlign: "center", padding: "0 20px" }}>
-                        <StyledButton
-                            className="border-style-btn"
-                            onClick={() => {
-                                fetchProducts(order.field, order.direction, true);
-                            }}
-                        >
-                            더보기
-                        </StyledButton>
-                    </div>
-                )}
+                <Category category={category} pathname={pathname} />
+                <ProductList pathname={pathname} />
             </section>
-            {pop.content}
             <MoveTop />
         </main>
     );
-}
+};
 
 export default ProductMain;
