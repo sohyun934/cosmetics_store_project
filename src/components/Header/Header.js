a27936a3d794beac31f988979a11212912ab2772
김소현 <a6570407@naver.com>
2022-12-02 15:45:01 +0900

    Refactor: "헤더 코드 리팩토링"
    가독성 향상

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,48 +6,60 @@ const react_1 = require("react");
 const react_router_dom_1 = require("react-router-dom");
 const auth_1 = require("firebase/auth");
 const firebase_1 = require("../../firebase");
-function LeftGnb() {
-    return ((0, jsx_runtime_1.jsx)("nav", Object.assign({ className: "header-gnb-left" }, { children: (0, jsx_runtime_1.jsxs)("ul", { children: [(0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: "/hair" }, { children: "\uD5E4\uC5B4\uCF00\uC5B4" })) }), (0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: "/skin" }, { children: "\uC2A4\uD0A8\uCF00\uC5B4" })) }), (0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: "/body" }, { children: "\uBC14\uB514\uCF00\uC5B4" })) })] }) })));
-}
-function SideGnbBtn(props) {
-    return ((0, jsx_runtime_1.jsxs)("button", Object.assign({ className: "side-gnb-btn", "aria-label": "navigation menu", onClick: () => props.activeSideGnb() }, { children: [(0, jsx_runtime_1.jsx)("div", { className: "line1" }), (0, jsx_runtime_1.jsx)("div", { className: "line2" }), (0, jsx_runtime_1.jsx)("div", { className: "line3" })] })));
-}
-function SideGnb(props) {
-    const isLoggedIn = props.isLoggedIn;
+const LeftNavBar = (props) => {
     const navigate = (0, react_router_dom_1.useNavigate)();
+    const navList = props.navList;
+    const navItems = navList.map(navItem => ((0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: navItem.link_to }, { children: navItem.title })) }, navItem.title)));
+    const isLogined = props.isLogined;
+    const logOut = props.logOut;
+    const closeGnb = props.closeGnb;
+    const toggleSideGnb = props.toggleSideGnb;
     const handleLog = (e) => {
-        if (isLoggedIn)
-            props.logOut(e);
-        else
+        if (isLogined) {
+            logOut(e);
+        }
+        else {
             navigate("/login", { state: { moveTo: -1 } });
-        props.closeGnb();
+        }
+        closeGnb();
     };
-    return ((0, jsx_runtime_1.jsx)("nav", Object.assign({ className: "side-gnb" }, { children: (0, jsx_runtime_1.jsxs)("ul", { children: [(0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: "/hair" }, { children: "\uD5E4\uC5B4\uCF00\uC5B4" })) }), (0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: "/skin" }, { children: "\uC2A4\uD0A8\uCF00\uC5B4" })) }), (0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: "/body" }, { children: "\uBC14\uB514\uCF00\uC5B4" })) }), (0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)("button", Object.assign({ className: "border-style-btn", style: { marginTop: "20px" }, onClick: handleLog }, { children: isLoggedIn ? "로그아웃" : "로그인" })) })] }) })));
-}
-function RightGnb(props) {
-    const isLoggedIn = props.isLoggedIn;
-    return ((0, jsx_runtime_1.jsx)("nav", Object.assign({ className: "header-gnb-right" }, { children: (0, jsx_runtime_1.jsxs)("ul", Object.assign({ className: "flex" }, { children: [isLoggedIn && ((0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)("a", Object.assign({ href: "/", onClick: e => props.logOut(e), style: { verticalAlign: "middle" } }, { children: "LOGOUT" })) })), (0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: isLoggedIn ? "/mypage/myPageAuthentification" : "/login", state: { moveTo: "/" } }, { children: (0, jsx_runtime_1.jsx)("img", { src: require("../../assets/common/mypage.png"), alt: "\uB9C8\uC774\uD398\uC774\uC9C0" }) })) }), (0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: isLoggedIn ? "/mypage/cart" : "/login", state: { moveTo: "/mypage/cart" } }, { children: (0, jsx_runtime_1.jsx)("img", { src: require("../../assets/common/cart.png"), alt: "\uC7A5\uBC14\uAD6C\uB2C8" }) })) })] })) })));
-}
-function Header() {
+    return ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [(0, jsx_runtime_1.jsx)("nav", Object.assign({ className: "header-gnb-left" }, { children: (0, jsx_runtime_1.jsx)("ul", { children: navItems }) })), (0, jsx_runtime_1.jsxs)("button", Object.assign({ className: "side-gnb-btn", "aria-label": "navigation menu", onClick: () => toggleSideGnb() }, { children: [(0, jsx_runtime_1.jsx)("div", { className: "line1" }), (0, jsx_runtime_1.jsx)("div", { className: "line2" }), (0, jsx_runtime_1.jsx)("div", { className: "line3" })] })), (0, jsx_runtime_1.jsx)("nav", Object.assign({ className: "side-gnb" }, { children: (0, jsx_runtime_1.jsxs)("ul", { children: [navItems, (0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)("button", Object.assign({ className: "border-style-btn", style: { marginTop: "20px" }, onClick: handleLog }, { children: isLogined ? "로그아웃" : "로그인" })) })] }) }))] }));
+};
+const RightNavBar = (props) => {
+    const logOut = props.logOut;
+    const isLogined = props.isLogined;
+    return ((0, jsx_runtime_1.jsx)("nav", Object.assign({ className: "header-gnb-right" }, { children: (0, jsx_runtime_1.jsxs)("ul", Object.assign({ className: "flex" }, { children: [isLogined && ((0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)("a", Object.assign({ href: "/", onClick: e => logOut(e), style: { verticalAlign: "middle" } }, { children: "LOGOUT" })) })), (0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: isLogined ? "/mypage/myPageAuthentification" : "/login", state: { moveTo: "/" } }, { children: (0, jsx_runtime_1.jsx)("img", { src: require("../../assets/common/mypage.png"), alt: "\uB9C8\uC774\uD398\uC774\uC9C0" }) })) }), (0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: isLogined ? "/mypage/cart" : "/login", state: { moveTo: "/mypage/cart" } }, { children: (0, jsx_runtime_1.jsx)("img", { src: require("../../assets/common/cart.png"), alt: "\uC7A5\uBC14\uAD6C\uB2C8" }) })) })] })) })));
+};
+const Header = () => {
     const navigate = (0, react_router_dom_1.useNavigate)();
     const pathname = (0, react_router_dom_1.useLocation)().pathname;
+    const navList = [
+        { link_to: "/hair", title: "헤어케어" },
+        { link_to: "/skin", title: "스킨케어" },
+        { link_to: "/body", title: "바디케어" }
+    ];
     const [toggle, setToggle] = (0, react_1.useState)(false);
-    const [isLoggedIn, setIsLoggedIn] = (0, react_1.useState)(false);
+    const [isLogined, setIsLogined] = (0, react_1.useState)(false);
+    (0, react_1.useEffect)(() => {
+        if (toggle) {
+            setToggle(false);
+        }
+    }, [pathname]);
     (0, react_1.useEffect)(() => {
         (0, auth_1.onAuthStateChanged)(firebase_1.auth, user => {
             if (user) {
-                setIsLoggedIn(true);
+                setIsLogined(true);
             }
             else {
-                setIsLoggedIn(false);
+                // 로그아웃, 세션 종료 시
+                setIsLogined(false);
             }
         });
     }, []);
-    function logOut(e) {
+    const logOut = (e) => {
         e.preventDefault();
         (0, auth_1.signOut)(firebase_1.auth)
             .then(() => {
-            setIsLoggedIn(false);
             // 마이페이지, 주문 페이지에서 로그아웃 시 메인 페이지로 이동
             if (pathname.indexOf("mypage") !== -1 || pathname.indexOf("order") !== -1)
                 navigate("/", { replace: true });
@@ -55,7 +67,7 @@ function Header() {
             .catch(error => {
             alert("로그아웃 과정 중에 오류가 발생했습니다.\n" + error.message);
         });
-    }
-    return ((0, jsx_runtime_1.jsxs)("header", Object.assign({ className: toggle ? "header side-active" : "header" }, { children: [(0, jsx_runtime_1.jsx)(LeftGnb, {}), (0, jsx_runtime_1.jsx)(SideGnbBtn, { activeSideGnb: () => setToggle(!toggle) }), (0, jsx_runtime_1.jsx)(SideGnb, { isLoggedIn: isLoggedIn, logOut: logOut, closeGnb: () => setToggle(false) }), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "logo" }, { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: "/" }, { children: (0, jsx_runtime_1.jsx)("img", { src: require("../../assets/common/logo.png"), alt: "\uB85C\uACE0" }) })) })), (0, jsx_runtime_1.jsx)(RightGnb, { isLoggedIn: isLoggedIn, logOut: logOut })] })));
-}
+    };
+    return ((0, jsx_runtime_1.jsxs)("header", Object.assign({ className: toggle ? "header side-active" : "header" }, { children: [(0, jsx_runtime_1.jsx)(LeftNavBar, { navList: navList, isLogined: isLogined, logOut: logOut, closeGnb: () => setToggle(false), toggleSideGnb: () => setToggle(!toggle) }), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "logo" }, { children: (0, jsx_runtime_1.jsx)(react_router_dom_1.Link, Object.assign({ to: "/" }, { children: (0, jsx_runtime_1.jsx)("img", { src: require("../../assets/common/logo.png"), alt: "\uB85C\uACE0" }) })) })), (0, jsx_runtime_1.jsx)(RightNavBar, { isLogined: isLogined, logOut: logOut })] })));
+};
 exports.default = Header;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,153 +4,149 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../../firebase";
 
-function LeftGnb() {
-    return (
-        <nav className="header-gnb-left">
-            <ul>
-                <li>
-                    <Link to="/hair">헤어케어</Link>
-                </li>
-                <li>
-                    <Link to="/skin">스킨케어</Link>
-                </li>
-                <li>
-                    <Link to="/body">바디케어</Link>
-                </li>
-            </ul>
-        </nav>
-    );
-}
-
-type Prop = {
-    activeSideGnb: Function;
-};
-
-function SideGnbBtn(props: Prop) {
-    return (
-        <button className="side-gnb-btn" aria-label="navigation menu" onClick={() => props.activeSideGnb()}>
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
-        </button>
-    );
-}
-
-type SideGnbProp = {
-    isLoggedIn: boolean;
+type LeftNavBarProp = {
+    navList: any[];
+    isLogined: boolean;
     logOut: Function;
     closeGnb: Function;
+    toggleSideGnb: Function;
 };
 
-function SideGnb(props: SideGnbProp) {
-    const isLoggedIn = props.isLoggedIn;
+const LeftNavBar = (props: LeftNavBarProp) => {
     const navigate = useNavigate();
 
+    const navList = props.navList;
+    const navItems = navList.map(navItem => (
+        <li key={navItem.title}>
+            <Link to={navItem.link_to}>{navItem.title}</Link>
+        </li>
+    ));
+
+    const isLogined = props.isLogined;
+    const logOut = props.logOut;
+    const closeGnb = props.closeGnb;
+    const toggleSideGnb = props.toggleSideGnb;
+
     const handleLog = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if (isLoggedIn) props.logOut(e);
-        else navigate("/login", { state: { moveTo: -1 } });
-        props.closeGnb();
+        if (isLogined) {
+            logOut(e);
+        } else {
+            navigate("/login", { state: { moveTo: -1 } });
+        }
+        closeGnb();
     };
 
     return (
-        <nav className="side-gnb">
-            <ul>
-                <li>
-                    <Link to="/hair">헤어케어</Link>
-                </li>
-                <li>
-                    <Link to="/skin">스킨케어</Link>
-                </li>
-                <li>
-                    <Link to="/body">바디케어</Link>
-                </li>
-                <li>
-                    <button className="border-style-btn" style={{ marginTop: "20px" }} onClick={handleLog}>
-                        {isLoggedIn ? "로그아웃" : "로그인"}
-                    </button>
-                </li>
-            </ul>
-        </nav>
+        <>
+            <nav className="header-gnb-left">
+                <ul>{navItems}</ul>
+            </nav>
+            <button className="side-gnb-btn" aria-label="navigation menu" onClick={() => toggleSideGnb()}>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </button>
+            <nav className="side-gnb">
+                <ul>
+                    {navItems}
+                    <li>
+                        <button className="border-style-btn" style={{ marginTop: "20px" }} onClick={handleLog}>
+                            {isLogined ? "로그아웃" : "로그인"}
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </>
     );
-}
+};
 
-type RightGnbProp = {
-    isLoggedIn: boolean;
+type RightNavBarProp = {
+    isLogined: boolean;
     logOut: Function;
 };
 
-function RightGnb(props: RightGnbProp) {
-    const isLoggedIn = props.isLoggedIn;
+const RightNavBar = (props: RightNavBarProp) => {
+    const logOut = props.logOut;
+    const isLogined = props.isLogined;
 
     return (
         <nav className="header-gnb-right">
             <ul className="flex">
-                {isLoggedIn && (
+                {isLogined && (
                     <li>
-                        <a href="/" onClick={e => props.logOut(e)} style={{ verticalAlign: "middle" }}>
+                        <a href="/" onClick={e => logOut(e)} style={{ verticalAlign: "middle" }}>
                             LOGOUT
                         </a>
                     </li>
                 )}
                 <li>
-                    <Link to={isLoggedIn ? "/mypage/myPageAuthentification" : "/login"} state={{ moveTo: "/" }}>
+                    <Link to={isLogined ? "/mypage/myPageAuthentification" : "/login"} state={{ moveTo: "/" }}>
                         <img src={require("../../assets/common/mypage.png")} alt="마이페이지" />
                     </Link>
                 </li>
                 <li>
-                    <Link to={isLoggedIn ? "/mypage/cart" : "/login"} state={{ moveTo: "/mypage/cart" }}>
+                    <Link to={isLogined ? "/mypage/cart" : "/login"} state={{ moveTo: "/mypage/cart" }}>
                         <img src={require("../../assets/common/cart.png")} alt="장바구니" />
                     </Link>
                 </li>
             </ul>
         </nav>
     );
-}
+};
 
-function Header() {
+const Header = () => {
     const navigate = useNavigate();
     const pathname = useLocation().pathname;
+    const navList = [
+        { link_to: "/hair", title: "헤어케어" },
+        { link_to: "/skin", title: "스킨케어" },
+        { link_to: "/body", title: "바디케어" }
+    ];
+
     const [toggle, setToggle] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLogined, setIsLogined] = useState(false);
+
+    useEffect(() => {
+        if (toggle) {
+            setToggle(false);
+        }
+    }, [pathname]);
 
     useEffect(() => {
         onAuthStateChanged(auth, user => {
             if (user) {
-                setIsLoggedIn(true);
+                setIsLogined(true);
             } else {
-                setIsLoggedIn(false);
+                // 로그아웃, 세션 종료 시
+                setIsLogined(false);
             }
         });
     }, []);
 
-    function logOut(e: React.MouseEvent) {
+    const logOut = (e: React.MouseEvent) => {
         e.preventDefault();
 
         signOut(auth)
             .then(() => {
-                setIsLoggedIn(false);
-
                 // 마이페이지, 주문 페이지에서 로그아웃 시 메인 페이지로 이동
                 if (pathname.indexOf("mypage") !== -1 || pathname.indexOf("order") !== -1) navigate("/", { replace: true });
             })
             .catch(error => {
                 alert("로그아웃 과정 중에 오류가 발생했습니다.\n" + error.message);
             });
-    }
+    };
 
     return (
         <header className={toggle ? "header side-active" : "header"}>
-            <LeftGnb />
-            <SideGnbBtn activeSideGnb={() => setToggle(!toggle)} />
-            <SideGnb isLoggedIn={isLoggedIn} logOut={logOut} closeGnb={() => setToggle(false)} />
+            <LeftNavBar navList={navList} isLogined={isLogined} logOut={logOut} closeGnb={() => setToggle(false)} toggleSideGnb={() => setToggle(!toggle)} />
             <div className="logo">
                 <Link to="/">
                     <img src={require("../../assets/common/logo.png")} alt="로고" />
                 </Link>
             </div>
-            <RightGnb isLoggedIn={isLoggedIn} logOut={logOut} />
+            <RightNavBar isLogined={isLogined} logOut={logOut} />
         </header>
     );
-}
+};
 
 export default Header;
