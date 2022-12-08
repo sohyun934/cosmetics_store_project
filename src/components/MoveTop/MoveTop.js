d1e1719ad5412afdb6f650bc3142c797e8bfdf92
김소현 <a6570407@naver.com>
2022-12-08 15:36:03 +0900

    Refactor: "MoveTop 컴포넌트 리팩토링"
    React.memo 적용

--- src/components/MoveTop/MoveTop.js
+++ src/components/MoveTop/MoveTop.js
@@ -1,8 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
+const react_1 = __importDefault(require("react"));
 require("./MoveTop.css");
-function MoveTop() {
+const MoveTop = react_1.default.memo(() => {
     return ((0, jsx_runtime_1.jsx)("div", Object.assign({ className: "move-top" }, { children: (0, jsx_runtime_1.jsx)("button", { "aria-label": "scroll to top button", onClick: () => window.scrollTo(0, 0) }) })));
-}
+});
 exports.default = MoveTop;
--- src/components/MoveTop/MoveTop.tsx
+++ src/components/MoveTop/MoveTop.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import "./MoveTop.css";
 
-function MoveTop() {
+const MoveTop = React.memo(() => {
     return (
         <div className="move-top">
             <button aria-label="scroll to top button" onClick={() => window.scrollTo(0, 0)}></button>
         </div>
     );
-}
+});
 
 export default MoveTop;
